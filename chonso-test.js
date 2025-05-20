cb14199b62f688e3faec2507738458a7a0395542
MobiFone7 <duong.tranvan@mobifone.vn>
2025-05-20 11:55:17 +0700

    up

---
+++ chonso-test.js
@@ -0,0 +1,78 @@
+const { Client } = require('@elastic/elasticsearch');
+require('dotenv').config(); // Đọc biến môi trường từ .env
+const client = new Client({
+    node: 'http://localhost:9200',
+    auth: {
+        username: process.env.ELASTIC_USER,
+        password: process.env.ELASTIC_PASSWORD
+    },
+    tls: {
+        rejectUnauthorized: false  // Bỏ kiểm tra SSL nếu dùng self-signed cert
+    }
+});
+
+async function checkConnection() {
+    try {
+        const response = await client.info();
+        console.dir(response, { depth: null });
+        console.log("BODY:", response.body);
+    } catch (err) {
+        console.error("Lỗi kết nối:", err.meta?.body || err);
+    }
+}
+
+async function searchAll() {
+    try {
+        const result = await client.search({
+            index: "chonso",
+            query: {
+                match_all: {}
+            },
+            size: 10  // Số lượng kết quả trả về (mặc định chỉ là 10)
+        });
+
+        console.log('📦 Kết quả:', result.hits.hits);
+    } catch (err) {
+        console.error('❌ Lỗi khi query:', err);
+    }
+}
+
+async function searchCondition() {
+    try {
+        const result = await client.search({
+            index: "chonso",
+            query: {
+                wildcard: {
+                    TEL_NUMBER: {
+                        value: '*7'
+                    }
+                }
+            },
+            size: 10  // Số lượng kết quả trả về (mặc định chỉ là 10)
+        });
+
+        console.log('📦 Kết quả:', result.hits.hits);
+    } catch (err) {
+        console.error('❌ Lỗi khi query:', err);
+    }
+}
+
+
+async function getCount() {
+    try {
+        const result = await client.count({
+            index: 'chonso'
+        });
+
+        console.log(`📊 Tổng số documents: ${result.count}`);
+    } catch (err) {
+        console.error('❌ Lỗi khi đếm documents:', err);
+    }
+}
+
+
+searchAll();
+// searchCondition();
+// checkConnection();
+// getCount();
+
